426457aa732074887b0d87dcaae414d4bb434c3a
dillan-p <dpisavadia20@gmail.com>
2018-09-17 22:36:43 +0100

    Create ui class for user interaction

--- lib/console/console_input.rb
+++ lib/console/console_input.rb
@@ -4,8 +4,9 @@ class InvalidInputError < StandardError; end
 
 module ConsoleInput
   def user_input(valid_range)
+    print 'Choice: '
     input = gets.chomp
     return input.to_i if valid_range.map(&:to_s).include?(input)
-    raise InvalidInputError, input
+    raise InvalidInputError.new("\n#{input} is not a valid option\n\n")
   end
 end
