c3df983211c07de067496d535aa2527a5dd7610f
dillan-p <dpisavadia20@gmail.com>
2018-09-17 19:15:07 +0100

    Create runner class that starts the game and edit game class to reflect changes

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,14 @@
 # frozen_string_literal: true
 
-require_relative 'player/computer_player'
-require_relative 'player/human_player'
 require_relative 'board'
 
 class Game
   attr_reader :board
 
-  def initialize(
-    com = ComputerPlayer.new,
-    hum = HumanPlayer.new,
-    board = Board.new
-  )
+  def initialize(player1, player2, board = Board.new)
     @board = board
-    @com = com
-    @hum = hum
+    @player1 = player1
+    @player2 = player2
   end
 
   def start_game
@@ -23,8 +17,8 @@ class Game
     puts 'Enter [0-8]:'
     # loop through until the game was won or tied
     until game_is_over?
-      make_move(@hum)
-      make_move(@com) unless game_is_over?
+      make_move(@player1)
+      make_move(@player2) unless game_is_over?
       puts " #{@board.grid[0]} | #{@board.grid[1]} | #{@board.grid[2]} \n===+===+===\n #{@board.grid[3]} | #{@board.grid[4]} | #{@board.grid[5]} \n===+===+===\n #{@board.grid[6]} | #{@board.grid[7]} | #{@board.grid[8]} \n"
     end
     puts 'Game over'
@@ -32,8 +26,10 @@ class Game
 
   def make_move(player)
     loop do
-      spot = player.get_spot(@board.grid, @hum.piece)
-      break if @board.set_piece(spot, player.piece)
+      # temp workaround to get piece before computer player changed
+      piece = player == @player1 ? @player1.piece : @player2.piece
+      spot = player.get_spot(@board.grid, piece)
+      break if @board.set_piece(spot, piece)
     end
   end
 
@@ -43,6 +39,3 @@ class Game
     @board.winner? || @board.tie?
   end
 end
-
-game = Game.new
-game.start_game
